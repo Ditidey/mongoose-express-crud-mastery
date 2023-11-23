60f075125f753ed40331347ed2bbe930d09f230c
Diti Rani Dey <ditidey2017@gmail.com>
2023-11-23 17:29:37 +0000

    server added

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import orderRoutes from './routes/orderRoutes';
 // import cors from 'cors';
 
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
-
+import app from '../app'
 const MONGODB_URI = "mongodb+srv://mongoose-mastery:Sxqpfp7atSVlZPMi@cluster0.kos6m2u.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI as string, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    } as mongoose.ConnectOptions);
+    await mongoose.connect(MONGODB_URI as string);
+    app.listen("server is running 5000")
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
