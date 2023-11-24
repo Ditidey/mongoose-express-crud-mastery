001cfc4901d8c238060d977ce7bc343f129df6b4
Diti Rani Dey <ditidey2017@gmail.com>
2023-11-24 11:51:05 +0000

    cors added

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, { Application } from 'express';
 import connectDB from './config/db';
 import userRoutes from './routes/userRoutes';
 import orderRoutes from './routes/orderRoutes';
-// import cors from 'cors';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-// app.use(cors())
+app.use(cors())
 
 // Connect to MongoDB
 connectDB();
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-const MONGODB_URI = "mongodb+srv://user-order:DfwYrf5lCPZg5wTE@cluster0.kos6m2u.mongodb.net/?retryWrites=true&w=majority";
-
+import dotenv from 'dotenv';
+// const MONGODB_URI = "mongodb+srv://user-order:DfwYrf5lCPZg5wTE@cluster0.kos6m2u.mongodb.net/user-order?retryWrites=true&w=majority";
+// mongodb+srv://<username>:<password>@cluster0.kos6m2u.mongodb.net/?retryWrites=true&w=majority
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI as string);
-    // console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);  
